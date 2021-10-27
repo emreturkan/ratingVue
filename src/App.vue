c3c7f819611a9d9b0e4c5eba024725c6819a9c1a
emre <emreturkan10@gmail.com>
2021-10-27 09:55:23 +0300

    updated App

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <router-link
-    :to="{ name: 'HomePage' }"
+  <a
+    href="https://www.instagram.com/p/CBgCheMDHOZ/"
     class="bg-red-400 w-24 h-8 flex justify-center items-center p-2 mt-4 ml-4 rounded text-white shadow-md cursor-pointer"
-    >Satın Al</router-link
+    >Satın Al</a
   >
   <router-view></router-view>
 </template>

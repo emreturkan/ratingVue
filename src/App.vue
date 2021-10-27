f7cc5c59ce05f76e4da1b33b05c520433c904550
emre <emreturkan10@gmail.com>
2021-10-27 09:32:15 +0300

    appV

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <router-link
     :to="{ name: 'HomePage' }"
     class="bg-red-400 w-24 h-8 flex justify-center items-center p-2 mt-4 ml-4 rounded text-white shadow-md cursor-pointer"
-    >Ana Sayfa</router-link
+    >Satın Al</router-link
   >
   <router-view></router-view>
 </template>
